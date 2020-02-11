e68316fdb023e6bf4acf39e5ea1c5901f2e891cd
Your Name <you@example.com>
2020-02-11 10:30:46 +0000

    pipes

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -10,15 +10,16 @@ export class AppComponent {
   model = {
     user: 'Daw',
     items: [
-      {action: "estudiar daw", done: false, prioridad: 4},
-      {action: "ayudar a mami", done: false, prioridad: 3},
-      {action: "ver Netflix", done: true, prioridad: 8},
-      {action: "recoger", done: false, prioridad: 2},
+      {id: "aaaaabbbbb", action: "estudiar daw", done: false, prioridad: 4},
+      {id: "bbbbbccccc", action: "ayudar a mami", done: false, prioridad: 3},
+      {id: "cccccddddd", action: "ver Netflix", done: true, prioridad: 8},
+      {id: "dddddeeeee", action: "recoger", done: false, prioridad: 2},
     ]
   };
+  mostrarTodas=true;
 
   constructor() {
-    this.ordenaTareas();
+   // this.ordenaTareas();
   }
 
   TnIncompletas(){
@@ -30,21 +31,22 @@ export class AppComponent {
 
   addItem(tarea){
     //console.log(tarea);
-    this.model.items.push({action: tarea, done: false, prioridad: 4});
-    this.ordenaTareas();
+    let nuevoId=Math.random().toString(36).substr(2,10);
+    this.model.items.push({id: nuevoId, action: tarea, done: false, prioridad: 4});
+   // this.ordenaTareas();
   }
 
-  nuevaPrioridad($event: any,i) {
+  nuevaPrioridad($event: any,id) {
     //console.log(i);
+    let i= this.model.items.findIndex((item:any)=>item.id==id, id);
     this.model.items[i].prioridad=$event;
   }
 
-  ordenaTareas(){
+/*  ordenaTareas(){
     this.model.items.sort((a:any,b:any)=>{
       if(a.action.toLowerCase()<b.action.toLowerCase()) return -1;
       else if (a.action.toLowerCase()>b.action.toLowerCase()) return 1;
       else return 0;
     })
-
-  }
+  }*/
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,11 +4,15 @@ import { NgModule } from '@angular/core';
 import { AppComponent } from './app.component';
 import {FormsModule} from '@angular/forms';
 import {PrioridadComponent} from './prioridad.component';
+import {OrdenaTareasPipe} from './ordenaTareas.pipe';
+import { FiltroDonePipe } from './filtro-done.pipe';
 
 @NgModule({
   declarations: [
     AppComponent,
-    PrioridadComponent
+    PrioridadComponent,
+    OrdenaTareasPipe,
+    FiltroDonePipe
   ],
   imports: [
     BrowserModule,
